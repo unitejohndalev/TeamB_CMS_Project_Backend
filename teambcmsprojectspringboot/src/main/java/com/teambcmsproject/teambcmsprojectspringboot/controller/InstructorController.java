f46e0d1c50779c1e6a4459712762f50944442273
Gem Ramirez <geraldinemaesramirez@gmail.com>
2024-01-15 14:22:32 +0800

    mapper and display data

--- teambcmsprojectspringboot/src/main/java/com/teambcmsproject/teambcmsprojectspringboot/controller/InstructorController.java
+++ teambcmsprojectspringboot/src/main/java/com/teambcmsproject/teambcmsprojectspringboot/controller/InstructorController.java
@@ -5,12 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.teambcmsproject.teambcmsprojectspringboot.model.Instructor;
 import com.teambcmsproject.teambcmsprojectspringboot.repository.InstructorRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
 
 @RestController
 @CrossOrigin("http://localhost:5173")
@@ -19,7 +24,7 @@ public class InstructorController {
     @Autowired
     private InstructorRepository instructorRepository;
      
-     @PostMapping("/instructor") //orginal user
+     @PostMapping("/createInstructor") //orginal user
     Instructor newInstructor(@RequestBody Instructor newInstructor){
       System.out.println(newInstructor.getInstructor_first_name());
       System.out.println(newInstructor.getInstructor_last_name());
@@ -29,11 +34,24 @@ public class InstructorController {
     return instructorRepository.save(newInstructor);
   }
 
-  @GetMapping("/instructors") //orginal users
+  @GetMapping("/getInstructor") //orginal users
   List<Instructor>getAllInstructors(){
     return instructorRepository.findAll();
 
 
   }
 
+ /*@GetMapping("/getInstructor/{id}")
+  public Instructor getInstructorById(@PathVariable Long instructor_id) {
+      return instructorRepository.findById(instructor_id)
+              .orElseThrow(() -> new UserNotfound());
+  }
+   */
+  @PutMapping("Instructor/{id}")
+    Instructor updateInstructor(@RequestBody Instructor newInstructor, @PathVariable Long instructor_id) {      
+      return instructorRepository.findById(instructor_id);
+      .map(instructor ->{
+        instructor.set
+      })
+  }
 }
